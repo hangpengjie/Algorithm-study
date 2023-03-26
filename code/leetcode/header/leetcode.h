2c1fd1f04d9705b703c685ad33aee0d9cbbcc8b5
杭朋洁 <596593218@qq.com>
2023-03-26 13:38:34 +0800

    增加线性欧拉筛函数

--- code/leetcode/header/leetcode.h
+++ code/leetcode/header/leetcode.h
@@ -8,5 +8,18 @@ typedef pair<int,int> pii;
 typedef pair<long long, long long> pll;
 typedef pair<ull,ull> pull;
 using namespace std;
-
+// CUT
+// 求[1,n]的所有质数 欧拉筛
+    void countPrime(int n, vector<int> &isPrime){
+        vector<int> prime(n+1, 1);
+        for(int i=2; i<=n; ++i){
+            if(prime[i])    isPrime.push_back(i);
+            for(int j=0; j <isPrime.size() && i * isPrime[j] <= n; ++j){
+                prime[isPrime[j] * i] = 0;
+                if(i % isPrime[j] == 0) break;
+            }
+        }
+        return; 
+    }
+// END CUT
 #endif
