c3b844b403b91a2a93206d1e87f91472acee9c21
hangpengjie <596593218@qq.com>
2023-03-31 14:51:26 +0800

    简单的每日一题

---
+++ code/leetcode/src/2367.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+// https://leetcode.cn/problems/number-of-arithmetic-triplets/
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] - nums[i] != diff) {
+                    continue;
+                }
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[k] - nums[j] == diff) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
