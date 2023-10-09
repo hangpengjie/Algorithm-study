d192891bcc6eddbd22f145319600c8f797375fa0
杭朋洁 <596593218@qq.com>
2023-10-09 22:25:40 +0800

    [模板] 增加最长公共子序列模板🍔

---
+++ code/subsequence/longest_common_subsequence.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+// # define MULTI
+// # define INIT
+using namespace std;
+# define endl '\n'
+# define space ' '
+# define LL long long
+# define PII pair<int, int>
+// by hangpengjie
+
+// 最长公共子序列
+// START CUT
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.length(), n = text2.length();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    for (int i = 1; i <= m; i++) {
+        char c1 = text1.at(i - 1);
+        for (int j = 1; j <= n; j++) {
+            char c2 = text2.at(j - 1);
+            if (c1 == c2) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+// END CUT
+
